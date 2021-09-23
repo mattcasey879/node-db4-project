c962fc81488bfc38d8dc13a89081e3a562b6700c
Matthew Casey <mattcasey879@gmail.com>
2021-09-22 20:00:21 -0500

    got the forth table up

--- data/migrations/20210922234820_first-migration.js
+++ data/migrations/20210922234820_first-migration.js
@@ -1,33 +1,42 @@
-
-exports.up = function(knex) {
-    return knex.schema
-        .createTable('recipes', tbl => {
-            tbl.increments("recipe_id")
-            tbl.string("recipe_name",128).notNullable.unique()
-            tbl.timestamp("created_at").defaultTo(knex.fn.now())
-        })
-        .createTable("ingredients", tbl => {
-            tbl.increments("ingr_id")
-            tbl.string("ingr_name",128).notNullable()
-            tbl.float("quanity")
-        })
-        .createTable('steps', table => {
-            table.increments('step_id')
-            table.string('step_instructions', 150).notNullable()
-            table.integer('recipe_id')
-              .unsigned()
-              .notNullable()
-              .references('recipe_id')
-              .inTable('recipe')
-              .onDelete('RESTRICT')
-              .onUpdate('RESTRICT')
-          })
-          .createTable("recipe_steps", tbl => {
-
-          })
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable.unique();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingr_id");
+      tbl.string("ingr_name", 128).notNullable();
+      tbl.float("quanity");
+    })
+    .createTable("steps", (table) => {
+      table.increments("step_id");
+      table.string("step_instructions", 150).notNullable();
+      table.integer("step_order").notNullable();
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipe")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("step_ingredents", (tbl) => {
+      tbl.increments("stIng_id");
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+     tbl.float("quanity")
+    });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-  .dropTableIfExists("recipes")
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("recipes");
 };
